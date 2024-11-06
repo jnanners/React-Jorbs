b47d07793c89f9ce87d71c2de614e84d88bb6802
JBanana <johnandrewharris3@gmail.com>
2024-11-06 16:38:42 -0600

    messing with variables

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,22 @@ const App = () => {
   const name = 'John';
   const x = 10;
   const y = 20;
+  const names = ['Brad', 'Mary', 'Joe', 'Lisa'];
 
 
   return (
     <div>
-      <div className='text-5xl'>App</div>
-      <p>My name is {name} and I am almost {x + y}</p>
+      <div className='text-5xl'>
+        App
+      </div>
+      <p>
+        My name is {name} and I am almost {x + y}
+      </p>
+      <ul>
+        {names.map((name) => (
+          <li>{name} is cool</li>
+        ))}
+      </ul>
     </div>
   )
 }
