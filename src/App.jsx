0a3cf865f33b234814becae937280f73f17e1df3
JBanana <johnandrewharris3@gmail.com>
2024-11-06 11:24:27 -0600

    git branching practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const App = () => {
+
+  const name = 'John';
+  const x = 10;
+  const y = 20;
+
+
   return (
-    <div className='text-5xl'>App</div>
+    <div>
+      <div className='text-5xl'>App</div>
+      <p>My name is {name} and I am almost {x + y}</p>
+    </div>
   )
 }
 
