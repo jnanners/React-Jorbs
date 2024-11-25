4944dff87b3fb68e45ddaf0d963198241e94a695
JBanana <johnandrewharris3@gmail.com>
2024-11-25 16:15:13 -0600

    added post and delete and toastify

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,46 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
-      <Route path='/add-job' element={<AddJobPage />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  )
-)
-
 const App = () => {
+  // Add Job
+  const addJob = async (newJob) => {
+    try {
+      const res = await fetch("/api/jobs", {
+        method: "POST",
+        headers: {
+          "Content-Type": "applications/json"
+        },
+        body: JSON.stringify(newJob),
+      })
+    } catch (error) {
+      console.log(error);
+    };
+    return;
+  }
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    try {
+      const res = await fetch(`/api/jobs/${id}`, {
+        method: "DELETE",
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return  <RouterProvider router={router} />
 };
 
